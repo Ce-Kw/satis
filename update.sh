c986762b383805935c3bd1e8edca3461d69f0fee
Kevin Wellmann <kevin@wellmann.io>
2020-11-04 20:41:36 +0100

    update script

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git rm -f composer.json composer.lock README.md satis.json update.sh
 echo $foo > satis.tmp.json
 php $(pwd)/vendor/bin/satis build satis.tmp.json .
 git rm satis.tmp.json -f
-git add .
+git add all.json index.html packages.json
 git commit -m "update dependencies"
 git push origin gh-pages
 git rm -f all.json index.html packages.json
