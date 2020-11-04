158cf3f9f62cc609d810176155d9938b80eb171d
Kevin Wellmann <kevin@wellmann.io>
2020-11-04 19:35:32 +0100

    update script

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 foo="`cat satis.json`"
 git checkout gh-pages
+git rm -rf .
 echo $foo > satis.json
 php $(pwd)/vendor/bin/satis build satis.json dist
-git commit -a -m "update dependencies"
+cd dist
+git add .
+git commit -m "update dependencies"
 git push origin gh-pages
 git checkout main
