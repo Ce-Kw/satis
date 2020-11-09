0051cdb68629570669ece556bc6d9d831a1f5552
Kevin Wellmann <kevin@wellmann.io>
2020-11-09 20:35:16 +0100

    update script

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ git add all.json index.html packages.json
 cd p2 && git add . && cd ..
 git status
 git commit -m "update dependencies"
-git push origin gh-pages
+git push origin gh-pages -f
 git rm -rf all.json index.html packages.json p2
 git checkout main
