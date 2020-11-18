c3866e2f75e1bec879b5bded37626d655b8d12a4
Kevin Wellmann <kevin@wellmann.io>
2020-11-18 19:58:33 +0100

    update dependencies

--- update.sh
+++ update.sh
@@ -1,9 +1,4 @@
-git checkout -B gh-pages
-php $(pwd)/vendor/bin/satis build satis.json .
-git add all.json index.html packages.json
-cd p2 && git add . && cd ..
-git status
+php $(pwd)/vendor/bin/satis build satis.json docs
+git add .
 git commit -m "update dependencies"
-git push origin gh-pages -f
-git rm -rf all.json index.html packages.json p2
-git checkout main
+git push origin main -f
